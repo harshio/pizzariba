f508a064819a881393f0f28648c0feeddfc46bdc
harshio <harshioleti@gmail.com>
2025-05-29 16:00:59 -0400

    comment with plan for an alteration to Props to account for making a full order description to place in the backend along with the total price

--- src/Order.tsx
+++ src/Order.tsx
@@ -8,7 +8,7 @@ interface Props {
 function Order({orders}: Props) {
     let totalSum = 0;
     for (const id in orders) {
-        const [quantity, price] = orders[Number(id)];
+        const [quantity, price] = orders[Number(id)]; //Note that for each id, we have the quantity ordered and its price. Furthermore, we have another data structure in Menu.tsx that maps food name to id. Perhaps when we implement backend functionality here, we could begin by making it so you have to pass in a second JSON object in component 'call's so that Menu can pass in initialItems, and we can use that to come up with the full order description.
         totalSum += quantity * price;
     }
     return (
